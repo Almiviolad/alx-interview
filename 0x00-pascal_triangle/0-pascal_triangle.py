d82b7f52d5c076637d56de920cd7c310e94e6165
root <mykelaloh@gmail.com>
2023-09-28 18:21:55 -0700

    pascal triangke task

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,16 @@
+""" A funcrion that rreturns a list of lists of integers representing the Pascal’s triangle"""
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = []
+    for i in range(n):
+        row = [1]  # The first element in each row is always 1
+        if triangle:
+            last_row = triangle[-1]
+            for j in range(len(last_row) - 1):
+                row.append(last_row[j] + last_row[j + 1])
+            row.append(1)  # The last element in each row is always 1
+        triangle.append(row)
+
+    return triangle
